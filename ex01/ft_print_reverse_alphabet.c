e6033e87449c6c3e9bb3f9e93ce907d04f75aab6
Nsuku Chauke <nschauke@c4r5s6.wethinkcode.co.za>
2020-07-09 15:22:19 +0200

    Second Commit

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+void  ft_putchar(char c) {
+	
+	write(1, &c, 1);
+}
+
+void ft_print_reverse_alphabet(void) {
+
+
+char c = 'z';
+while (c >= 'a')
+			{
+				ft_putchar(c);
+				c--;
+			}
+
+
+
+}
+int  main(void) {
+	ft_print_reverse_alphabet();
+
+	return(0);
+}
+
